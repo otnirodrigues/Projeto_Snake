3576aaaafe6f95d884b1ef764a864a7a6305a84a
Otni Rodrigues <otnirodrigues@gmail.com>
2022-01-26 01:35:46 -0300

    Tentando fazer a snake mudar de posição

--- Snake.py
+++ Snake.py
@@ -1,4 +1,3 @@
-
 import random
 import pygame
 from pygame import *
@@ -8,6 +7,7 @@ class Apllication():
     
     # Metodo que inicia o pygame e chama os outros metodos
     def __init__(self):
+       
        pygame.init()
        self.criando_janela()
        self.criando_snake()
@@ -28,6 +28,7 @@ class Apllication():
         self.snake = [(200,200), (210,200), (220,200)]
         self.snake_skin = pygame.Surface((10, 10))
         self.snake_skin.fill((255, 255, 255))
+        self.snake_direction = pygame.KEYUP
         
         
     
@@ -45,37 +46,12 @@ class Apllication():
 
         # Limitando a velocidade do FPS da Snake
         clock = pygame.time.Clock()
-        clock.tick(2)
+        clock.tick(20)
 
-
-    def mover_snake(self):
-
-        # Importando o library turtle para capturar as ações
-        # 'penup' == UP, 'right' == RIGHT, 'pendown' == DOWN, 'left' == LEFT
-        # Setando a direção da Snake
-        
-        UP = 0
-        RIGHT = 1
-        DOWN = 2
-        LEFT = 3
-
-        
-
-        if self.snake_direction == UP:
-            self.snake[0] = (self.snake[0][0], self.snake[0][1] -10)
-        if self.snake_direction == DOWN:
-            self.snake[0] = (self.snake[0][0], self.snake[0][1] +10)
-        if self.snake_direction == LEFT:
-            self.snake[0] = (self.snake[0][0] - 10, self.snake[0][1])
-        if self.snake_direction == RIGHT:
-            self.snake[0] = (self.snake[0][0] + 10, self.snake[0][1] -10)
-            
-        for i in range(len(self.snake) -1, 0, -1):
-            self.snake[i] = (self.snake[i-1][0], self.snake[i-1][1])
 
     
     def game(self):
-        self.snake_direction = 3
+        
         while True:
             
             # Captura os eventos que seram realizados
@@ -84,15 +60,28 @@ class Apllication():
                 if event.type == QUIT:
                     pygame.quit()
 
-            for event in pygame.event.get():
-                if event.type == KEYUP:
+                if event.type == pygame.KEYUP:
+                    self.snake[0] = (self.snake[0][0], self.snake[0][1] -10)
+                if event.type == pygame.KEYDOWN:
+                    self.snake[0] = (self.snake[0][0], self.snake[0][1] +10)
+                if event.type == pygame.K_LEFT:
+                    self.snake[0] = (self.snake[0][0] - 10, self.snake[0][1])
+                if event.type == pygame.K_RIGHT:
+                    self.snake[0] = (self.snake[0][0] + 10, self.snake[0][1] -10)
+            
+                for i in range(len(self.snake) -1, 0, -1):
+                    self.snake[i] = (self.snake[i-1][0], self.snake[i-1][1])
+    
+
+            ''' for event in pygame.event.get():
+                if event.type == pygame.KEYUP:
                     self.snake_direction = self.mover_snake()
-                if event.type == KEYDOWN:
+                if event.type == pygame.KEYDOWN:
                     self.snake_direction = self.mover_snake()
-                if event.type == K_LEFT:
+                if event.type == pygame.K_LEFT:
                     self.snake_direction = self.mover_snake
-                if event.type == K_RIGHT:
-                    self.snake_direction = self.mover_snake() 
+                if event.type == pygame.K_RIGHT:
+                    self.snake_direction = self.mover_snake() ''' 
                 
 
 
