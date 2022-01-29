0cd6ab8614dcb4965a7667f409ebb2dec76c9f62
Otni Rodrigues <otnirodrigues@gmail.com>
2022-01-28 21:39:58 -0300

    adicionando um metodo para o game over

--- Snake.py
+++ Snake.py
@@ -1,7 +1,8 @@
 import random
-from tkinter import LEFT, RIGHT
+from webbrowser import get
 import pygame
 from pygame import *
+from pyparsing import White
 
 
 class Apllication():
@@ -11,10 +12,10 @@ class Apllication():
     DOWN = 2
     LEFT = 3
     snake_direction = LEFT
+    cont = 0
 
     # Metodo que inicia o pygame e chama os outros metodos
     def __init__(self):
-       
        pygame.init()
        self.criando_janela()
        self.criando_snake()
@@ -22,7 +23,6 @@ class Apllication():
        self.game()
 
     def criando_janela(self):
-        
         # Criando uma janela de tamanho 600x600, e alterando o nome da janela para 'Snake'
         self.screen = pygame.display.set_mode((600, 600))
         pygame.display.set_caption('Snake')
@@ -35,14 +35,11 @@ class Apllication():
         self.snake_skin = pygame.Surface((10, 10))
         self.snake_skin.fill((255, 255, 255))
         
-        
-    
     def update_snake_matriz(self):
         # Atualizando a snake na matriz
         for pos in self.snake:
             self.screen.blit(self.snake_skin, pos)
 
-    
     def criando_maca(self):
         # Setando um objeto maça, de tamanho 10x10
         # Setando a cor da Maça
@@ -51,8 +48,8 @@ class Apllication():
         self.pos_maca = self.setando_maca_grid()
         
     def setando_maca_grid(self):
-        #setando uma posição na matriz aleatoriamente, o escopo está dentro de 0 à 590, pois a matriz vai de 0 à 600
-        
+        # setando uma posição na matriz aleatoriamente, o escopo está dentro de 0 à 590, pois a matriz vai de 0 à 600
+        # Tratando x e y, para que retorne um numero inteiro entre 0 e 590. // divide e arredonda para int
         x = random.randint(0, 590)
         y = random.randint(0, 590)
         return(x//10 * 10, y//10 * 10)
@@ -63,26 +60,43 @@ class Apllication():
         
 
     def movimento_snake(self):
-
         # Metodo para definir regras de movimento snake
+        for i in range(len(self.snake) -1, 0, -1):
+            self.snake[i] = (self.snake[i-1][0], self.snake[i-1][1])
         if self.snake_direction == self.UP:
             self.snake[0] = (self.snake[0][0], self.snake[0][1] -10)
         if self.snake_direction == self.DOWN:
             self.snake[0] = (self.snake[0][0], self.snake[0][1] +10)
         if self.snake_direction == self.LEFT:
-            self.snake[0] = (self.snake[0][1] - 10, self.snake[0][0])
+            self.snake[0] = (self.snake[0][0] - 10, self.snake[0][1])
         if self.snake_direction == self.RIGHT:
-            self.snake[0] = (self.snake[0][1] + 10, self.snake[0][0])
-        for i in range(len(self.snake) -1, 0, -1):
-            self.snake[i] = (self.snake[i-1][0], self.snake[i-1][1])
-
+            self.snake[0] = (self.snake[0][0] + 10, self.snake[0][1])
+       
 
     def fps_snake(self):
-
         # Limitando a velocidade do FPS da Snake
-        clock = pygame.time.Clock()
-        clock.tick(5)
-    
+        self.clock = pygame.time.Clock()
+        self.clock.tick(15)
+
+    def game_over(self):
+        # Definindo os limites da janela, caso ultrapasse, game over
+        if self.snake[0][0] == 600 or self.snake[0][1] == 600 or self.snake[0][0] <0 or self.snake[0][1] < 0:
+            
+            # Laço para criar um novo display com um uma menssagem de game over
+            while True:  
+                screen_over = pygame.display.set_mode((600, 400))  
+                font = pygame.font.SysFont("comicsansms", 70)
+                text = font.render("Game Over", True, (0, 128, 0))           
+                pygame.display.set_caption("Snake - Game Over")         
+                screen_over.blit(text, (300 - text.get_width() // 2, 200 - text.get_height() // 2))
+                screen_over.fill((255,255,255))        
+                self.clock.tick(15)
+                pygame.display.flip()
+                for event in pygame.event.get():
+                    if event.type == pygame.QUIT:
+                        pygame.quit()
+                pygame.display.update()
+
     def game(self):
         
         while True:
@@ -107,12 +121,14 @@ class Apllication():
                         self.snake_direction = self.RIGHT
 
             self.movimento_snake()
+            self.game_over()
 
             # Tratando a colisão entre snake e maça
             # Gera uma nova posição para maça
             if self.colisao(self.snake[0], self.pos_maca):
                 self.pos_maca = self.setando_maca_grid()
                 self.snake.append((0,0))
+                self.cont += 1 
             
             # Limpando a tela
             # Protando a maça na matrix
